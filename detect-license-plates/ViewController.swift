ad8b5813ab62d253ec603a45bd38124d64d2b4ca
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2019-11-17 18:56:13 +0100

    Initial Commit

---
+++ detect-license-plates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  detect-license-plates
+//
+//  Created by M'haimdat omar on 17-11-2019.
+//  Copyright © 2019 M'haimdat omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
