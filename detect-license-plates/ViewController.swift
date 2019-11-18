d08959957aae76ae6f26925f0fb8fc2d20b73435
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2019-11-18 20:44:05 +0100

    First commit

--- detect-license-plates/ViewController.swift
+++ detect-license-plates/ViewController.swift
@@ -7,14 +7,199 @@
 //
 
 import UIKit
+import Vision
+import AVKit
+import CoreMedia
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    // MARK: - Vision Properties
+    var request: VNCoreMLRequest?
+    var OcrRequest: VNRecognizeTextRequest?
+    var visionModel: VNCoreMLModel?
+    var isInferencing = false
+    
+    // MARK: - AV Property
+    var videoCapture: VideoCapture!
+    let semaphore = DispatchSemaphore(value: 1)
+    
+    let videoPreview: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let BoundingBoxView: DrawingBoundingBoxView = {
+       let boxView = DrawingBoundingBoxView()
+        boxView.translatesAutoresizingMaskIntoConstraints = false
+        return boxView
+    }()
 
+    let identifierLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .green
+        label.font = UIFont(name: "Avenir", size: 30)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        setUpModel()
+        setupLabel()
+        setupCameraView()
+        setUpCamera()
+        setupBoundingBoxView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.videoCapture.start()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.videoCapture.stop()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        resizePreviewLayer()
+    }
+    
+    func resizePreviewLayer() {
+        videoCapture.previewLayer?.frame = videoPreview.bounds
+    }
+    
+    // MARK: - Setup CoreML model and Text Request recognizer
+    func setUpModel() {
+        if let visionModel = try? VNCoreMLModel(for: model_plate_turi().model) {
+            self.visionModel = visionModel
+            request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
+            OcrRequest = VNRecognizeTextRequest(completionHandler: visionTextRequestDidComplete)
+            request?.imageCropAndScaleOption = .scaleFill
+        } else {
+            fatalError("fail to create vision model")
+        }
     }
 
+    // MARK: - SetUp Camera preview
+    func setUpCamera() {
+        videoCapture = VideoCapture()
+        videoCapture.delegate = self
+        videoCapture.fps = 30
+        videoCapture.setUp(sessionPreset: .vga640x480) { success in
+            
+            if success {
+                // add preview view on the layer
+                if let previewLayer = self.videoCapture.previewLayer {
+                    self.videoPreview.layer.addSublayer(previewLayer)
+                    self.resizePreviewLayer()
+                }
+                
+                // start video preview when setup is done
+                self.videoCapture.start()
+            }
+        }
+    }
+    
+    fileprivate func setupCameraView() {
+        view.addSubview(videoPreview)
+        videoPreview.bottomAnchor.constraint(equalTo: identifierLabel.topAnchor).isActive = true
+        videoPreview.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        videoPreview.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        videoPreview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    }
+    
+    fileprivate func setupBoundingBoxView() {
+        view.addSubview(BoundingBoxView)
+        BoundingBoxView.bottomAnchor.constraint(equalTo: videoPreview.bottomAnchor).isActive = true
+        BoundingBoxView.leftAnchor.constraint(equalTo: videoPreview.leftAnchor).isActive = true
+        BoundingBoxView.rightAnchor.constraint(equalTo: videoPreview.rightAnchor).isActive = true
+        BoundingBoxView.topAnchor.constraint(equalTo: videoPreview.topAnchor).isActive = true
+    }
+    
+    fileprivate func setupLabel() {
+        view.addSubview(identifierLabel)
+        identifierLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32).isActive = true
+        identifierLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        identifierLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        identifierLabel.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
 
+}
+
+extension ViewController: VideoCaptureDelegate {
+    func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame pixelBuffer: CVPixelBuffer?, timestamp: CMTime) {
+        // the captured image from camera is contained on pixelBuffer
+        if !self.isInferencing, let pixelBuffer = pixelBuffer {
+            self.isInferencing = true
+            // predict!
+            self.predictUsingVision(pixelBuffer: pixelBuffer)
+        }
+    }
+}
+
+extension ViewController {
+    func predictUsingVision(pixelBuffer: CVPixelBuffer) {
+        guard let request = request else { fatalError() }
+        guard let OcrRequest = OcrRequest else {fatalError()}
+        OcrRequest.recognitionLevel = .fast
+        // vision framework configures the input size of image following our model's input configuration automatically which is 416X416
+        self.semaphore.wait()
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+        try? handler.perform([request, OcrRequest])
+    }
+    
+    // MARK: - Post-processing
+    func visionRequestDidComplete(request: VNRequest, error: Error?) {
+        if let predictions = request.results as? [VNRecognizedObjectObservation] {
+            DispatchQueue.main.async {
+                self.BoundingBoxView.predictedObjects = predictions
+                self.isInferencing = false
+            }
+        } else {
+            
+            self.isInferencing = false
+        }
+        
+        let request = VNRecognizeTextRequest { (request, error) in
+        guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+        for currentObservation in observations {
+            let topCandidate = currentObservation.topCandidates(1)
+                if let recognizedText = topCandidate.first {
+                    DispatchQueue.main.async {
+                        self.identifierLabel.text = recognizedText.string
+                    }
+                }
+                
+            }
+        }
+        
+        request.recognitionLevel = .fast
+        self.semaphore.signal()
+    }
+    
+    func visionTextRequestDidComplete(request: VNRequest, error: Error?) {
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            for currentObservation in observations {
+                let topCandidate = currentObservation.topCandidates(1)
+                    if let recognizedText = topCandidate.first {
+                        DispatchQueue.main.async {
+                            self.identifierLabel.text = recognizedText.string
+                        }
+                    }
+                    
+                }
+            self.semaphore.signal()
+        }
 }
 
